fddaa6d142c2a60cf053ab4ec30856c4180e852a
Luca Battesini <lucabattesini1@gmail.com>
2024-01-18 21:01:26 -0300

    Adding an input with the package downloaded

---
+++ menu.js
@@ -0,0 +1,14 @@
+var prompt = require('prompt-sync')();
+
+const escolha = prompt(`
+Escolha uma opção
+
+==================================
+
+0 - Sair
+1 - Cadastre filmes
+2 - listar filmes
+3 - Calcular média
+4 - Filtrar filmes por não vistos
+`
+);
