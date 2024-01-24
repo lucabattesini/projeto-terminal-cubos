93a44cae11571d4bcbcaee704b4b3760d9767221
Luca Battesini <lucabattesini1@gmail.com>
2024-01-23 22:18:35 -0300

    Creating a file to create new functions

--- menu.js
+++ menu.js
@@ -22,19 +22,4 @@ if ( escolha === 0 )  {
 
 } else if ( escolha === 1 ) {
 
-    const nomeNovoFilme = prompt("Digite o nome do filme: ");
-    const rottenTomatoNovoFilme = Number(prompt("Digite a nota do filme no Rotten Tomato: "));
-    const imdbNovoFilme = Number(prompt("Digite a nota do filme no Imdb: "));
-    const assistidoNovoFilme = prompt("Você já assistiu este filme?: ");
-
-    const novoFilme = {
-        nome: nomeNovoFilme,
-        nota: {
-            rottenTomato: rottenTomatoNovoFilme,
-            imdb: imdbNovoFilme
-        },
-        assistido: assistidoNovoFilme
-    }
-
-    filmes.push(novoFilme)
 }
