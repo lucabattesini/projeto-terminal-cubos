de7bde593e323dbd871dfaa1e078abb691a47714
Luca Battesini <lucabattesini1@gmail.com>
2024-01-23 22:20:07 -0300

    Creating a function that save a new moovie to the db

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
-export function cadastrarFilme() {
-    
+export function cadastrarFilme(filmes) {
+    const nomeNovoFilme = prompt("Digite o nome do filme: ");
+    const rottenTomatoNovoFilme = Number(prompt("Digite a nota do filme no Rotten Tomato: "));
+    const imdbNovoFilme = Number(prompt("Digite a nota do filme no Imdb: "));
+    const assistidoNovoFilme = prompt("Você já assistiu este filme?: ");
+
+    const novoFilme = {
+        nome: nomeNovoFilme,
+        nota: {
+            rottenTomato: rottenTomatoNovoFilme,
+            imdb: imdbNovoFilme
+        },
+        assistido: assistidoNovoFilme
+    }
+
+    filmes.push(novoFilme)
 }
