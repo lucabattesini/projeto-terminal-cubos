818ffc7877e3103f2767c6444d29f08218e30490
Luca Battesini <lucabattesini1@gmail.com>
2024-01-28 09:59:20 -0300

    Adding just 1 movie to the aplication

--- menu.js
+++ menu.js
@@ -8,39 +8,7 @@ const filmes = [
         imdb: 9.6
     },
      assistido: "não"
-    },
-    {
-    nome: "Hora de Aventura",
-    nota: {
-        rottenTomato: 10,
-        imdb: 9.9
-    },
-     assistido: "sim"
-    },
-    {
-    nome: "Batman",
-    nota: {
-        rottenTomato: 9.9,
-        imdb: 9.6
-    },
-     assistido: "sim"
-    },
-    {
-    nome: "500 days of summer",
-    nota: {
-        rottenTomato: 10,
-        imdb: 10
-    },
-     assistido: "sim"
-    },
-    {
-    nome: "Interstellar",
-    nota: {
-        rottenTomato: 9.8,
-        imdb: 8.9
-    },
-     assistido: "sim"
-    },
+    }
 ];
 
 
