4fd350850f86bbe34da56b19e33bfd27102e76f9
Luca Battesini <lucabattesini1@gmail.com>
2024-01-28 11:14:42 -0300

    puting everything in a while true

--- menu.js
+++ menu.js
@@ -9,65 +9,66 @@ const filmes = [
     }
 ];
 
+while (true) {
+    console.log(`
+    Escolha uma opção
 
-console.log(`
-Escolha uma opção
+    ==================================
 
-==================================
+    0 - Sair
+    1 - Cadastrar filmes
+    2 - listar filmes
+    3 - Calcular média
+    4 - Filtrar por filmes não vistos
+    `
+    );
 
-0 - Sair
-1 - Cadastrar filmes
-2 - listar filmes
-3 - Calcular média
-4 - Filtrar por filmes não vistos
-`
-);
+    const escolha = Number(prompt(" "))
 
-const escolha = Number(prompt(" "))
+    if ( escolha === 0 )  {
+        console.log("Aplicação fechada")
 
-if ( escolha === 0 )  {
-    console.log("Aplicação fechada")
+    } else if ( escolha === 1 ) {
+        const nomeNovoFilme = prompt("Digite o nome do filme: ");
+        const rottenTomatoNovoFilme = Number(prompt("Digite a nota do filme no Rotten Tomato: "));
+        const imdbNovoFilme = Number(prompt("Digite a nota do filme no Imdb: "));
+        const assistidoNovoFilme = prompt("Você já assistiu este filme? [s/n]: ");
 
-} else if ( escolha === 1 ) {
-    const nomeNovoFilme = prompt("Digite o nome do filme: ");
-    const rottenTomatoNovoFilme = Number(prompt("Digite a nota do filme no Rotten Tomato: "));
-    const imdbNovoFilme = Number(prompt("Digite a nota do filme no Imdb: "));
-    const assistidoNovoFilme = prompt("Você já assistiu este filme? [s/n]: ");
-
-    const novoFilme = {
-        nome: nomeNovoFilme,
-        nota: {
-            rottenTomato: rottenTomatoNovoFilme,
-            imdb: imdbNovoFilme
-        },
-        assistido: assistidoNovoFilme
-    }
-
-    filmes.push(novoFilme)
-} else if (escolha === 2) {
-    console.table(filmes)
-
-} else if ( escolha === 3 ) {
-    const nomeFilmeEscolhido =  prompt("Digite o nome do filme desejado: ")
-
-    for ( item of filmes ) {
-        if ( item.nome === nomeFilmeEscolhido ) {
-            notaRottenTomatos = item.rottenTomato
-            notaImdb = item.imdb
-            somaNotas = notaRottenTomatos + notaImdb
-            mediaNotas = somaNotas / 2
-
-            console.log("A média das notas é " + mediaNotas)
-
-            break
+        const novoFilme = {
+            nome: nomeNovoFilme,
+            nota: {
+                rottenTomato: rottenTomatoNovoFilme,
+                imdb: imdbNovoFilme
+            },
+            assistido: assistidoNovoFilme
         }
-    }
-} else if ( escolha === 4 ) {
-    for ( item of filmes ){
-        if ( item.assistido === 'n' ) {
-            console.table(item)
+
+        filmes.push(novoFilme)
+    } else if (escolha === 2) {
+        console.table(filmes)
+
+    } else if ( escolha === 3 ) {
+        const nomeFilmeEscolhido =  prompt("Digite o nome do filme desejado: ")
+
+        for ( item of filmes ) {
+            if ( item.nome === nomeFilmeEscolhido ) {
+                notaRottenTomatos = item.rottenTomato
+                notaImdb = item.imdb
+                somaNotas = notaRottenTomatos + notaImdb
+                mediaNotas = somaNotas / 2
+
+                console.log("A média das notas é " + mediaNotas)
+
+                break
+            }
         }
+    } else if ( escolha === 4 ) {
+        for ( item of filmes ){
+            if ( item.assistido === 'n' ) {
+                console.table(item)
+            }
+        }
+    } else if ( escolha > 4 ) {
+        console.log("Escolha invalida")
     }
-} else if ( escolha > 4 ) {
-    console.log("Escolha invalida")
 }
